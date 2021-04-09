17e4b67d887cc5acc7a57c917d4fbe6ff7494f42
momeyer <meyer.monique93@gmail.com>
2021-04-09 12:36:32 +0200

    inproved player move func

--- src/player/player.c
+++ src/player/player.c
@@ -2,14 +2,6 @@
 #include <math.h>
 #include <utils/math_utils.h>
 
-static t_bool hit_wall(t_map worldMap, t_position new_pos)
-{
-	t_grid_position pos = to_grid_position(worldMap, new_pos);
-	if (worldMap.matrix[pos.y][pos.x] > 0)
-		return (TRUE);
-	return (FALSE);
-}
-
 t_player create_player()
 {
 	t_player player;
@@ -20,72 +12,69 @@ t_player create_player()
 	return (player);
 }
 
+static t_bool can_move(t_map worldMap, t_position new_pos)
+{
+	const t_grid_position pos = to_grid_position(worldMap, new_pos);
+
+	return (worldMap.matrix[pos.y][pos.x] == (int)EMPTY);
+}
+
+static t_bool move(t_player *player, t_map worldMap, t_position new_pos)
+{
+	if (!can_move(worldMap, new_pos))
+		return (FALSE);
+	player->position.y = new_pos.y;
+	player->position.x = new_pos.x;
+	return (TRUE);
+}
+
 t_bool move_left(t_player *player, t_map worldMap)
 {
-	t_position new_pos = player->position;
-	new_pos.x += floor(cos(degree_to_radians(player->angle + 90)) * player->speed);
-	new_pos.y -= floor(sin(degree_to_radians(player->angle + 90)) * player->speed);
-	if (!hit_wall(worldMap, new_pos))
-	{
-		player->position.y = new_pos.y;
-		player->position.x = new_pos.x;
-		return (TRUE);
-	}
-	return (FALSE);
+	const t_position new_pos = {
+		.x = (player->position.x +
+			  floor(cos(degree_to_radians(player->angle + 90)) * player->speed)),
+		.y =
+			player->position.y - floor(sin(degree_to_radians(player->angle + 90)) * player->speed)};
+
+	return (move(player, worldMap, new_pos));
 }
 
 t_bool move_right(t_player *player, t_map worldMap)
 {
-	t_position new_pos = player->position;
-	new_pos.x += floor(cos(degree_to_radians(player->angle - 90)) * player->speed);
-	new_pos.y -= floor(sin(degree_to_radians(player->angle - 90)) * player->speed);
-	if (!hit_wall(worldMap, new_pos))
-	{
-		player->position.y = new_pos.y;
-		player->position.x = new_pos.x;
-		return (TRUE);
-	}
-	return (FALSE);
+	const t_position new_pos = {
+		.x = player->position.x + floor(cos(degree_to_radians(player->angle - 90)) * player->speed),
+		.y =
+			player->position.y - floor(sin(degree_to_radians(player->angle - 90)) * player->speed)};
+
+	return (move(player, worldMap, new_pos));
 }
 
 t_bool move_forward(t_player *player, t_map worldMap)
 {
-	t_position new_pos = player->position;
-	new_pos.x += floor(cos(degree_to_radians(player->angle)) * player->speed);
-	new_pos.y -= floor(sin(degree_to_radians(player->angle)) * player->speed);
-	if (!hit_wall(worldMap, new_pos))
-	{
-		player->position.y = new_pos.y;
-		player->position.x = new_pos.x;
-		return (TRUE);
-	}
-	return (FALSE);
+	const t_position new_pos = {
+		.x = player->position.x + floor(cos(degree_to_radians(player->angle)) * player->speed),
+		.y = player->position.y - floor(sin(degree_to_radians(player->angle)) * player->speed)};
+
+	return (move(player, worldMap, new_pos));
 }
 
 t_bool move_backward(t_player *player, t_map worldMap)
 {
-	t_position new_pos = player->position;
-	new_pos.x -= floor(cos(degree_to_radians(player->angle)) * player->speed);
-	new_pos.y += floor(sin(degree_to_radians(player->angle)) * player->speed);
-	if (!hit_wall(worldMap, new_pos))
-	{
-		player->position.y = new_pos.y;
-		player->position.x = new_pos.x;
-		return (TRUE);
-	}
-	return (FALSE);
+	const t_position new_pos = {
+		.x = player->position.x - floor(cos(degree_to_radians(player->angle)) * player->speed),
+		.y = player->position.y + floor(sin(degree_to_radians(player->angle)) * player->speed)};
+
+	return (move(player, worldMap, new_pos));
 }
 
 void turn_counterclockwise(t_player *player)
 {
-	if (player->angle >= 360)
-		player->angle = 0;
+	player->angle = fix_angle(player->angle);
 	player->angle += 2.0;
 }
 
 void turn_clockwise(t_player *player)
 {
-	if (player->angle <= 0)
-		player->angle = 360;
+	player->angle = fix_angle(player->angle);
 	player->angle -= 2.0;
 }
